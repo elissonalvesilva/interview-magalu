a696d7dadfe05fb6b3a351aa691f9cd9f2d56654
Elisson Alves Silva <nossilesilva@gmail.com>
2021-02-28 02:31:22 -0400

    feat: ensure email validation

--- src/presentation/controllers/add-client-controller.ts
+++ src/presentation/controllers/add-client-controller.ts
@@ -1,9 +1,12 @@
 import { ok } from './../helpers/http-helpers';
-import { MissingParamError } from '../erros';
+import { InvalidParamError, MissingParamError } from '../erros';
 import { badRequest } from '../helpers/http-helpers';
 import { Controller, HttpResponse } from '../protocols';
+import { EmailValidator } from 'presentation/protocols/email-validator';
 
 export class AddClientController implements Controller {
+  constructor(private emailValidator: EmailValidator) {}
+
   async handle(request: any): Promise<HttpResponse> {
     const requiredFields = ['name', 'email'];
 
@@ -11,6 +14,11 @@ export class AddClientController implements Controller {
       if (!request.body[field]) {
         return badRequest(new MissingParamError(field));
       }
+    }
+
+    const isValidEmail = this.emailValidator.isValidEmail(request.email);
+    if (!isValidEmail) {
+      return badRequest(new InvalidParamError('email'));
     }
 
     return Promise.resolve(ok(true));
