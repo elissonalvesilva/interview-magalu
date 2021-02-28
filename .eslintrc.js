c0fd79d44aa17e609cba84cc28a05efdee77fdb5
Elisson Alves Silva <nossilesilva@gmail.com>
2021-02-28 00:43:00 -0400

    refactor: change required var to off

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
   },
 };
