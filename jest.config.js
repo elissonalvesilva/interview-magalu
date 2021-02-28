7d697c98bdadfebfa5b674f92e8459519c150f37
Elisson Alves Silva <nossilesilva@gmail.com>
2021-02-28 00:43:27 -0400

    feat: add jest and configure

--- jest.config.js
+++ jest.config.js
@@ -20,8 +20,7 @@ export default {
   // collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
-
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
@@ -31,7 +30,7 @@ export default {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
