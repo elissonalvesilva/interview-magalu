648758f0f181eaaf3f737894e3a06e58b6ab45d3
Elisson Alves Silva <nossilesilva@gmail.com>
2021-03-01 22:24:18 -0400

    test: ensure update client in application

---
+++ __tests__/application/use-cases/db-update-client.spec.ts
@@ -0,0 +1,120 @@
+import { DbUpdateClient } from './../../../src/application/use-cases';
+import { UpdateClientRepository } from 'application/protocols';
+import { Client } from 'domain/protocols';
+import { CheckClientByIdRepository } from 'application/protocols';
+
+const makeFakeClient = (): Client => ({
+  id: 'valid_id',
+  name: 'valid name',
+  email: 'mail@mail.com',
+});
+
+const makeCheckClientByIdRepository = (): CheckClientByIdRepository => {
+  class CheckClientByIdRepositoryStub implements CheckClientByIdRepository {
+    checkClientById(id: string): Promise<boolean> {
+      return new Promise((resolve) => resolve(true));
+    }
+  }
+  return new CheckClientByIdRepositoryStub();
+};
+
+const makeUpdateClientRepository = (): UpdateClientRepository => {
+  class UpdateClientRepositoryStub implements UpdateClientRepository {
+    updateClient(client: Client): Promise<boolean> {
+      return new Promise((resolve) => resolve(true));
+    }
+  }
+  return new UpdateClientRepositoryStub();
+};
+
+interface SutTypes {
+  sut: DbUpdateClient;
+  updateClientRepositoryStub: UpdateClientRepository;
+  checkClientByIdRepositoryStub: CheckClientByIdRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const updateClientRepositoryStub = makeUpdateClientRepository();
+  const checkClientByIdRepositoryStub = makeCheckClientByIdRepository();
+  const sut = new DbUpdateClient(
+    updateClientRepositoryStub,
+    checkClientByIdRepositoryStub,
+  );
+  return {
+    sut,
+    updateClientRepositoryStub,
+    checkClientByIdRepositoryStub,
+  };
+};
+
+describe('DbUpdateClient UseCase', () => {
+  describe('CheckClientByIdRepository', () => {
+    test('should call CheckClientByIdRepository with correct values', async () => {
+      const { sut, checkClientByIdRepositoryStub } = makeSut();
+
+      const checkClientByIdRepositorySpy = jest.spyOn(
+        checkClientByIdRepositoryStub,
+        'checkClientById',
+      );
+
+      await sut.update(makeFakeClient());
+      expect(checkClientByIdRepositorySpy).toBeCalledWith(makeFakeClient().id);
+    });
+
+    test('should return throw if CheckClientByIdRepository throws', async () => {
+      const { sut, checkClientByIdRepositoryStub } = makeSut();
+
+      jest
+        .spyOn(checkClientByIdRepositoryStub, 'checkClientById')
+        .mockReturnValueOnce(
+          new Promise((resolve, reject) => reject(new Error())),
+        );
+
+      const promise = sut.update(makeFakeClient());
+      await expect(promise).rejects.toThrow();
+    });
+  });
+
+  describe('UpdateClientRepository', () => {
+    test('should call UpdateClientRepository with correct values', async () => {
+      const {
+        sut,
+        updateClientRepositoryStub,
+        checkClientByIdRepositoryStub,
+      } = makeSut();
+
+      jest
+        .spyOn(checkClientByIdRepositoryStub, 'checkClientById')
+        .mockReturnValueOnce(new Promise((resolve) => resolve(false)));
+
+      const UpdateClientRepositorySpy = jest.spyOn(
+        updateClientRepositoryStub,
+        'updateClient',
+      );
+
+      await sut.update(makeFakeClient());
+      expect(UpdateClientRepositorySpy).toBeCalledWith(makeFakeClient());
+    });
+
+    test('should return throw if UpdateClientRepository throws', async () => {
+      const {
+        sut,
+        updateClientRepositoryStub,
+        checkClientByIdRepositoryStub,
+      } = makeSut();
+
+      jest
+        .spyOn(checkClientByIdRepositoryStub, 'checkClientById')
+        .mockReturnValueOnce(new Promise((resolve) => resolve(false)));
+
+      jest
+        .spyOn(updateClientRepositoryStub, 'updateClient')
+        .mockReturnValueOnce(
+          new Promise((resolve, reject) => reject(new Error())),
+        );
+
+      const promise = sut.update(makeFakeClient());
+      await expect(promise).rejects.toThrow();
+    });
+  });
+});
