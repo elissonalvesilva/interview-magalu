15c2aac3b6f6497ac0c3e1ac96c9013280bc76ca
Elisson Alves Silva <nossilesilva@gmail.com>
2021-03-02 22:10:40 -0400

    feat: ensure check client by id in infra

--- src/infrastructure/database/mongodb/client-mongo-repository.ts
+++ src/infrastructure/database/mongodb/client-mongo-repository.ts
@@ -1,12 +1,31 @@
 import { Client } from 'domain/protocols';
 
-import { AddClientRepository } from './../../../application/protocols/add-client-repository';
+import {
+  AddClientRepository,
+  CheckClientByIdRepository,
+} from './../../../application/protocols';
 import { MongoHelper } from './helpers';
 
-export class ClientMongoRepository implements AddClientRepository {
+export class ClientMongoRepository
+  implements AddClientRepository, CheckClientByIdRepository {
   async addClient(client: Client): Promise<boolean> {
     const clientCollection = await MongoHelper.getCollection('clients');
     const resultResponse = await clientCollection.insertOne(client);
     return resultResponse.ops[0] !== null;
+  }
+
+  async checkClientById(id: string): Promise<boolean> {
+    const clientCollection = await MongoHelper.getCollection('clients');
+    const account = await clientCollection.findOne(
+      {
+        _id: id,
+      },
+      {
+        projection: {
+          _id: 1,
+        },
+      },
+    );
+    return account !== null;
   }
 }
