7a88fb47ce479375b6080852b0c37ebe567766ae
Elisson Alves Silva <nossilesilva@gmail.com>
2021-03-02 22:18:30 -0400

    feat: add check client by id in infra

--- src/infrastructure/database/mongodb/client-mongo-repository.ts
+++ src/infrastructure/database/mongodb/client-mongo-repository.ts
@@ -2,12 +2,16 @@ import { Client } from 'domain/protocols';
 
 import {
   AddClientRepository,
+  CheckClientByEmailRepository,
   CheckClientByIdRepository,
 } from './../../../application/protocols';
 import { MongoHelper } from './helpers';
 
 export class ClientMongoRepository
-  implements AddClientRepository, CheckClientByIdRepository {
+  implements
+    AddClientRepository,
+    CheckClientByIdRepository,
+    CheckClientByEmailRepository {
   async addClient(client: Client): Promise<boolean> {
     const clientCollection = await MongoHelper.getCollection('clients');
     const resultResponse = await clientCollection.insertOne(client);
@@ -16,7 +20,7 @@ export class ClientMongoRepository
 
   async checkClientById(id: string): Promise<boolean> {
     const clientCollection = await MongoHelper.getCollection('clients');
-    const account = await clientCollection.findOne(
+    const client = await clientCollection.findOne(
       {
         _id: id,
       },
@@ -26,6 +30,22 @@ export class ClientMongoRepository
         },
       },
     );
-    return account !== null;
+    return client !== null;
+  }
+
+  async checkClientByEmail(email: string): Promise<boolean> {
+    const clientCollection = await MongoHelper.getCollection('clients');
+    const client = await clientCollection.findOne(
+      {
+        email,
+      },
+      {
+        projection: {
+          _id: 1,
+        },
+      },
+    );
+
+    return client !== null;
   }
 }
