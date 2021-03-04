36c06dc78d387668aa14bd649a334fc650ee5d36
Elisson Alves Silva <nossilesilva@gmail.com>
2021-03-03 21:53:43 -0400

    test: create validation if product already added

--- __tests__/application/use-cases/db-add-favorit-product-client.spec.ts
+++ __tests__/application/use-cases/db-add-favorit-product-client.spec.ts
@@ -1,11 +1,13 @@
 import {
   NotFoundClientResponse,
   NotFoundProductResponse,
+  ProductAlreadyAddedResponse,
 } from './../../../src/presentation/helpers/stack-errors-favorits';
 import { Product } from './../../../src/domain/protocols/product';
 import { DbAddFavoritProductClient } from './../../../src/application/use-cases/db-add-favorit-product-client';
 import {
   AddFavoritProductClientRepository,
+  CheckAddedFavoritProductRepository,
   CheckClientByIdRepository,
   ProductServiceRepository,
 } from './../../../src/application/protocols';
@@ -53,6 +55,16 @@ const makeproductServiceRepository = (): ProductServiceRepository => {
   return new ProductServiceRepositoryStub();
 };
 
+const makeCheckAddedProductRepository = (): CheckAddedFavoritProductRepository => {
+  class CheckAddedFavoritProductRepositoryStub
+    implements CheckAddedFavoritProductRepository {
+    checkProduct(productId: string): Promise<boolean> {
+      return new Promise((resolve) => resolve(true));
+    }
+  }
+  return new CheckAddedFavoritProductRepositoryStub();
+};
+
 const makeCheckClientByIdRepository = (): CheckClientByIdRepository => {
   class CheckClientByIdRepositoryStub implements CheckClientByIdRepository {
     checkClientById(id: string): Promise<boolean> {
@@ -67,14 +79,17 @@ interface SutTypes {
   checkClientByIdRepositoryStub: CheckClientByIdRepository;
   productServiceRepositoryStub: ProductServiceRepository;
   addFavoritProductClientRepositoryStub: AddFavoritProductClientRepository;
+  checkAddedFavoritProductRepositoryStub: CheckAddedFavoritProductRepository;
 }
 
 const makeSut = (): SutTypes => {
   const checkClientByIdRepositoryStub = makeCheckClientByIdRepository();
   const productServiceRepositoryStub = makeproductServiceRepository();
   const addFavoritProductClientRepositoryStub = makeAddFavoritProductClientRepository();
+  const checkAddedFavoritProductRepositoryStub = makeCheckAddedProductRepository();
   const sut = new DbAddFavoritProductClient(
     checkClientByIdRepositoryStub,
+    checkAddedFavoritProductRepositoryStub,
     productServiceRepositoryStub,
     addFavoritProductClientRepositoryStub,
   );
@@ -83,6 +98,7 @@ const makeSut = (): SutTypes => {
     checkClientByIdRepositoryStub,
     productServiceRepositoryStub,
     addFavoritProductClientRepositoryStub,
+    checkAddedFavoritProductRepositoryStub,
   };
 };
 
@@ -135,13 +151,47 @@ describe('DbAddFavoritProductClient UseCase', () => {
     });
   });
 
+  describe('CheckAddedFavoritProductRepository', () => {
+    test('should call CheckAddedFavoritProductRepository with correct values', async () => {
+      const { sut, checkAddedFavoritProductRepositoryStub } = makeSut();
+      const checkAddedFavoritProductRepositorySpy = jest.spyOn(
+        checkAddedFavoritProductRepositoryStub,
+        'checkProduct',
+      );
+      await sut.add(makeFakeClientId().id, makeFakeProductId().id);
+      expect(checkAddedFavoritProductRepositorySpy).toBeCalledWith(
+        makeFakeProductId().id,
+      );
+    });
+    test('should return throw if CheckAddedFavoritProductRepository throws', async () => {
+      const { sut, checkAddedFavoritProductRepositoryStub } = makeSut();
+      jest
+        .spyOn(checkAddedFavoritProductRepositoryStub, 'checkProduct')
+        .mockReturnValueOnce(
+          new Promise((resolve, reject) => reject(new Error())),
+        );
+      const promise = sut.add(makeFakeClientId().id, makeFakeProductId().id);
+      await expect(promise).rejects.toThrow();
+    });
+  });
+
   describe('AddFavoritProductClientRepository', () => {
     test('should call AddFavoritProductClientRepository with correct values', async () => {
-      const { sut, addFavoritProductClientRepositoryStub } = makeSut();
+      const {
+        sut,
+        addFavoritProductClientRepositoryStub,
+        checkAddedFavoritProductRepositoryStub,
+      } = makeSut();
+
+      jest
+        .spyOn(checkAddedFavoritProductRepositoryStub, 'checkProduct')
+        .mockReturnValueOnce(new Promise((resolve) => resolve(false)));
+
       const addFavoritProductClientRepositorySpy = jest.spyOn(
         addFavoritProductClientRepositoryStub,
         'addFavorit',
       );
+
       await sut.add(makeFakeClientId().id, makeFakeProductId().id);
       expect(addFavoritProductClientRepositorySpy).toBeCalledWith(
         makeFakeClientId().id,
@@ -149,7 +199,16 @@ describe('DbAddFavoritProductClient UseCase', () => {
       );
     });
     test('should return throw if AddFavoritProductClientRepository throws', async () => {
-      const { sut, addFavoritProductClientRepositoryStub } = makeSut();
+      const {
+        sut,
+        addFavoritProductClientRepositoryStub,
+        checkAddedFavoritProductRepositoryStub,
+      } = makeSut();
+
+      jest
+        .spyOn(checkAddedFavoritProductRepositoryStub, 'checkProduct')
+        .mockReturnValueOnce(new Promise((resolve) => resolve(false)));
+
       jest
         .spyOn(addFavoritProductClientRepositoryStub, 'addFavorit')
         .mockReturnValueOnce(
@@ -222,16 +281,52 @@ describe('DbAddFavoritProductClient UseCase', () => {
       expect(response.stack).toMatchObject(fakeStack);
     });
 
+    test('should error if product already added', async () => {
+      const {
+        sut,
+        checkClientByIdRepositoryStub,
+        checkAddedFavoritProductRepositoryStub,
+      } = makeSut();
+
+      jest
+        .spyOn(checkAddedFavoritProductRepositoryStub, 'checkProduct')
+        .mockReturnValueOnce(new Promise((resolve) => resolve(true)));
+
+      const fakeStack: ProductAlreadyAddedResponse = {
+        message: 'Product already added',
+        productid: makeFakeProductId().id,
+      };
+
+      jest
+        .spyOn(checkClientByIdRepositoryStub, 'checkClientById')
+        .mockReturnValueOnce(new Promise((resolve) => resolve(true)));
+
+      const response = await sut.add(
+        makeFakeClientId().id,
+        makeFakeProductId().id,
+      );
+      expect(response).toBeTruthy();
+      expect(response.added).toBeFalsy();
+      expect(response.clientid).toBe(makeFakeClientId().id);
+      expect(response.productid).toBe(makeFakeProductId().id);
+      expect(response.stack).toMatchObject(fakeStack);
+    });
+
     test('should success if all params exists', async () => {
       const {
         sut,
         checkClientByIdRepositoryStub,
         productServiceRepositoryStub,
+        checkAddedFavoritProductRepositoryStub,
       } = makeSut();
 
       jest
         .spyOn(checkClientByIdRepositoryStub, 'checkClientById')
         .mockReturnValueOnce(new Promise((resolve) => resolve(true)));
+
+      jest
+        .spyOn(checkAddedFavoritProductRepositoryStub, 'checkProduct')
+        .mockReturnValueOnce(new Promise((resolve) => resolve(false)));
 
       jest
         .spyOn(productServiceRepositoryStub, 'getProduct')
