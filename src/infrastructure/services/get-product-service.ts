4c82d6d0dbcbe72953fc1bb37bd284933509206f
Elisson Alves Silva <nossilesilva@gmail.com>
2021-03-05 16:19:24 -0400

    feat: create get product service in infra

---
+++ src/infrastructure/services/get-product-service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from './../../application/protocols';
+import { HttpResponse } from './../../presentation/protocols';
+import { ProductServiceRepository } from './../../application/protocols/product-service-repository';
+
+export class GetProductService implements ProductServiceRepository {
+  constructor(private urlAPI: string, private httpClient: HttpClient) {}
+
+  async getProduct(id: string): Promise<HttpResponse> {
+    const request = await this.httpClient.request({
+      method: 'get',
+      url: `${this.urlAPI}/${id}`,
+    });
+
+    return request;
+  }
+}
