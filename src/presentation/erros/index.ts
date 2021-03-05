1453f6ae3b494c0c81bd2308b1c59cc31c7e7e1e
Elisson Alves Silva <nossilesilva@gmail.com>
2021-03-04 23:07:02 -0400

    feat: add in index to export email error

--- src/presentation/erros/index.ts
+++ src/presentation/erros/index.ts
@@ -3,3 +3,4 @@ export * from './missing-param-error';
 export * from './server-error';
 export * from './not-found-param-error';
 export * from './bad-request-error';
+export * from './email-in-use-error';
