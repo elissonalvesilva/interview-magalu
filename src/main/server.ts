5c4ee3d3b960038970884bf5b5e8cb3916855594
Elisson Alves Silva <nossilesilva@gmail.com>
2021-03-04 21:34:20 -0400

    feat: create server and start api

---
+++ src/main/server.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import { MongoHelper } from './../infrastructure/database/mongodb/helpers/mongo-helper';
+
+dotenv.config();
+
+MongoHelper.connect(process.env.MONGO_URL || '')
+  .then(async () => {
+    const app = (await import('./app')).default;
+    app.listen(process.env.PORT, () =>
+      console.log(
+        `Wishilist API running at http://localhost:${process.env.PORT}`,
+      ),
+    );
+  })
+  .catch(console.error);
