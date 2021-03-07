d4f5264835b547094bc1bf6041449fa2372ddf8b
Elisson Alves Silva <nossilesilva@gmail.com>
2021-03-07 19:03:44 -0400

    test: create account mongo db and add method add

---
+++ __tests__/infrastructure/database/mongodb/account-mongo-repository.spec.ts
@@ -0,0 +1,38 @@
+import faker from 'faker';
+
+import { MongoHelper } from './../../../../src/infrastructure/database/mongodb/helpers/mongoose-helper';
+import { AccountMongoRepository } from './../../../../src/infrastructure/database/mongodb/account-mongo-repository';
+import AccountModel from './../../../../src/infrastructure/database/mongodb/models/Account';
+
+const makeFakeAccount = (): any => {
+  const password = faker.internet.password();
+  return {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password,
+  };
+};
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository();
+};
+
+describe('AccountMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL || '');
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    await AccountModel.deleteMany({});
+  });
+  test('Should return true if created account', async () => {
+    const sut = makeSut();
+    const fakeAccount = makeFakeAccount();
+    const isValid = await sut.add(fakeAccount);
+    expect(isValid).toBe(true);
+  });
+});
