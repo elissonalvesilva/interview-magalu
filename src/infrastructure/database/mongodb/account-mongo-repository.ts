b100474dc61b3a44f867dac19aec4725ba7a6181
Elisson Alves Silva <nossilesilva@gmail.com>
2021-03-07 19:09:42 -0400

    feat: ensure in account repo a method checkAccountByEmail

--- src/infrastructure/database/mongodb/account-mongo-repository.ts
+++ src/infrastructure/database/mongodb/account-mongo-repository.ts
@@ -1,10 +1,17 @@
+import { CheckAccountByEmailRepository } from './../../../application/protocols/account/check-account-by-email-repository';
 import { Account } from './../../../domain/protocols';
 import { AddAccountRepository } from './../../../application/protocols/account/add-account-repository';
 import AccountModel from './../mongodb/models/Account';
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+  implements AddAccountRepository, CheckAccountByEmailRepository {
   async add(account: Account): Promise<boolean> {
     const accountResp = await AccountModel.create(account);
     return accountResp !== null;
+  }
+
+  async checkAccountByEmail(email: string): Promise<boolean> {
+    const account = await AccountModel.findOne({ email });
+    return account !== null;
   }
 }
