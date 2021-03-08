447b078c44ddd29bf3806e6f91dbb7df14433b49
Elisson Alves Silva <nossilesilva@gmail.com>
2021-03-07 20:22:54 -0400

    feat: create authentication in application layer

--- src/application/protocols/account/index.ts
+++ src/application/protocols/account/index.ts
@@ -1,2 +1,4 @@
 export * from './add-account-repository';
 export * from './check-account-by-email-repository';
+export * from './get-account-by-email-repository';
+export * from './update-access-token-repository';
---
+++ src/application/use-cases/db-authentication.ts
@@ -0,0 +1,48 @@
+import {
+  GetAccountByEmailRepository,
+  UpdateAccessTokenRepository,
+} from './../../application/protocols/account';
+import {
+  CriptograpyEncrypter,
+  CriptograpyHashComparer,
+} from './../../application/protocols/criptography';
+import { Authentication } from './../../domain/use-cases/authentication';
+import {
+  AuthenticationParams,
+  AuthenticationResult,
+} from './../../domain/protocols';
+
+export class DbAuthentication implements Authentication {
+  constructor(
+    private readonly getAccountByEmailRepository: GetAccountByEmailRepository,
+    private readonly hashComparer: CriptograpyHashComparer,
+    private readonly encrypter: CriptograpyEncrypter,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository,
+  ) {}
+
+  async auth(
+    authenticationParams: AuthenticationParams,
+  ): Promise<AuthenticationResult | null> {
+    const account = await this.getAccountByEmailRepository.loadByEmail(
+      authenticationParams.email,
+    );
+    if (account) {
+      const isValid = await this.hashComparer.compare(
+        authenticationParams.password,
+        account.password,
+      );
+      if (isValid) {
+        const accessToken = await this.encrypter.encrypt(account._id);
+        await this.updateAccessTokenRepository.updateAccessToken(
+          account._id,
+          accessToken,
+        );
+        return {
+          accessToken,
+          name: account.name,
+        };
+      }
+    }
+    return null;
+  }
+}
